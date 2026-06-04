07471faad08f765d08195450b11f15cfad08e04e
Thoth9n <chiidensan@gmail.com>
2026-06-04 10:29:09 +0800

    fix JavaFX project merge issues

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -34,18 +34,4 @@ public class App extends Application {
     public static void main(String[] args) {
         launch();
     }
-
-import java.util.Scanner;
-
-public class App {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Kubiin irmeg: ");
-        long side = scanner.nextLong();
-
-        kub kub = new kub(side);
-        System.out.println("ezelhuun: " + kub.getVolume() + " " + "gadarguin talbai: " + kub.getSurfaceArea());
-        System.out.println("oorchlolt");
-        scanner.close();
-    }
 }
